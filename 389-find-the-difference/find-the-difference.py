de175388b715606104f99c2e7fca3633083b8061
keerthi konduru <71881753+Keerthi-2@users.noreply.github.com>
2023-09-25 09:46:00 +0530

    Time: 43 ms (47.34%) | Memory: 16.3 MB (43.09%) - LeetSync

--- 389-find-the-difference/find-the-difference.py
+++ 389-find-the-difference/find-the-difference.py
@@ -2,15 +2,12 @@ class Solution:
     def findTheDifference(dlf, s: str, t: str) -> str:
         if len(s)==0:
             return t
-        s1=list(s)
-        s2=list(t)
-        s1.sort()
-        s2.sort()
-        print(s1,s2)
-        for i in range(len(s1)):
-            if s1[i]!=s2[i]:
-                return s2[i]
-        return s2[len(s1)]
+        xor=0
+        for i in s:
+            xor=xor^ord(i)
+        for i in t:
+            xor=xor^ord(i)
+        return chr(xor)
 
 
         
