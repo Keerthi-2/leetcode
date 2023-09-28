4960a7c6fab786f7fe2f87de82aee19375ea1272
keerthi konduru <71881753+Keerthi-2@users.noreply.github.com>
2023-09-28 07:46:33 +0530

    Time: 64 ms (16.80%) | Memory: 16.2 MB (88.52%) - LeetSync

--- 2283-sort-even-and-odd-indices-independently/sort-even-and-odd-indices-independently.py
+++ 2283-sort-even-and-odd-indices-independently/sort-even-and-odd-indices-independently.py
@@ -10,8 +10,9 @@ class Solution:
             else:
                 even.append(nums[i])
                 
-        odd.sort(reverse=True)
+        odd.sort()
         even.sort()
+        o=len(odd)-1
         
         for i in range(len(nums)):
             if i%2==0:
@@ -19,7 +20,7 @@ class Solution:
                 e+=1
             else:
                 nums[i]=odd[o]
-                o+=1
+                o-=1
         return nums
                 
         
