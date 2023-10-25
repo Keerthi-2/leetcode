574d1a807b96fa4f113cdb8c0d2e07b0b95f6aab
Keerthi Konduru <71881753+Keerthi-2@users.noreply.github.com>
2023-10-25 10:22:09 +0530

    Time: 38 ms (57.09%) | Memory: 16.2 MB (50.00%) - LeetSync

---
+++ 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.py
@@ -0,0 +1,11 @@
+from math import pow
+class Solution:
+    def kthGrammar(self, n: int, k: int) -> int:
+        if n==1:
+            return 0
+        mid=int(pow(2,n-1))//2
+        if k<=mid:
+            return self.kthGrammar(n-1,k)
+        else:
+            return 1-self.kthGrammar(n-1,k-mid)
+        
