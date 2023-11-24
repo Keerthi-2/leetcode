10fa17ebd2e0179de301cc24ec14344a2f11a677
Keerthi Konduru <71881753+Keerthi-2@users.noreply.github.com>
2023-11-24 10:30:50 +0530

    Time: 48 ms (30.26%) | Memory: 16.6 MB (11.65%) - LeetSync

--- 413-arithmetic-slices/arithmetic-slices.py
+++ 413-arithmetic-slices/arithmetic-slices.py
@@ -7,11 +7,12 @@ class Solution:
         #  1 2 3 4 5 6       123 234 345 456  1234 2345 3456  12345 23456 123456
 
         res=0
-        prev=0
+        prev=1
         for i in range(2,len(nums)):
             if nums[i]-nums[i-1]==nums[i-1]-nums[i-2]:
-                prev+=1
+                
                 res+=prev
+                prev+=1
             else:
-                prev=0
+                prev=1
         return res
