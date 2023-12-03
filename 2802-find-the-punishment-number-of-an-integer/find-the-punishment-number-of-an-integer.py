ecc9cde3f1107d519f6e74728048e1396d6a4c2f
Keerthi Konduru <71881753+Keerthi-2@users.noreply.github.com>
2023-12-03 17:19:25 +0530

    Time: 2598 ms (7.85%) | Memory: 16.6 MB (12.97%) - LeetSync

--- 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.py
+++ 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.py
@@ -8,8 +8,8 @@ class Solution:
                     self.valid=True
                 return 
             for i in range(ind+1,len(s)+1):
-                left,right=int(s[ind:i]),(s[i+ind:])
-
+                left,right=int(s[ind:i]),(s[i:])
+                print(left,right)
                 if left<=t:
                     backtrack(t-left,i)
 
@@ -19,12 +19,11 @@ class Solution:
         res=0
 
         for i in range(1,n+1):
-            print("entered for loop")
             self.valid=False
             s=str(i*i)
             backtrack(i,0)
             if self.valid:
-                print(True)
+              
                 res+=i*i
         return res
         
