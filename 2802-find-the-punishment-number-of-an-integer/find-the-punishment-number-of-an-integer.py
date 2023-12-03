adc7b95eb60390eb301a9c7f2e396b1e01a8a5ac
Keerthi Konduru <71881753+Keerthi-2@users.noreply.github.com>
2023-12-03 17:12:29 +0530

    Time: 1249 ms (57.68%) | Memory: 16.3 MB (35.49%) - LeetSync

--- 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.py
+++ 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.py
@@ -1,27 +1,30 @@
 class Solution:
     def punishmentNumber(self, n: int) -> int:
-        def backtrack(i, target):
-            # Given a string s and int target, return True if s can be partitioned into contiguous substrings s.t. sum of the integer
-            # values of the substrings equals target.
-            # Backtrack pruning: don't need to consider splitting if splitting results in some number > target
-            
-            if i == len(s):
-                if target == 0:
-                    self.valid = True
-                return
-            
-            for j in range(i + 1, len(s) + 1):
-                # try to partition s[:j] and s[j:]
-                left, right = s[i:j], s[i + j:]
-                if int(left) <= target:
-                    backtrack(j, target - int(left))
-        
-        res = 0
-        for i in range(1, n + 1):
-            self.valid = False
-            s = str(i * i)
-            backtrack(0, i)
+
+        def backtrack(t,ind):
+
+            if ind==len(s):
+                if t==0:
+                    self.valid=True
+                return 
+            for i in range(ind+1,len(s)+1):
+                left,right=int(s[ind:i]),(s[i+ind:])
+
+                if left<=t:
+                    backtrack(t-left,i)
+
+
+
+
+        res=0
+
+        for i in range(1,n+1):
+            print("entered for loop")
+            self.valid=False
+            s=str(i*i)
+            backtrack(i,0)
             if self.valid:
-                res += i * i
-        
+                print(True)
+                res+=i*i
         return res
+        
