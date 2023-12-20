7af3a9e6fd87b3773f0660927abc34bff877d8c0
Keerthi Konduru <71881753+Keerthi-2@users.noreply.github.com>
2023-12-20 10:50:04 +0530

    Time: 77 ms (7.73%) | Memory: 16.4 MB (23.41%) - LeetSync

--- 2756-buy-two-chocolates/buy-two-chocolates.py
+++ 2756-buy-two-chocolates/buy-two-chocolates.py
@@ -2,16 +2,27 @@ class Solution:
     def buyChoco(self, prices: List[int], money: int) -> int:
         
         
-        amount=money
-        count=0
-        s=0
-        first_min=min(prices)
-        prices.remove(first_min)
-        second_min=min(prices)
-        s+=first_min+second_min
         
+        s=0
+        
+        freq=[0]*100
+
+        for i in prices:
+            freq[i-1]+=1
+        print(freq)
+        first_min,second_min=0,0
+        for i in range(1,101):
+            if first_min==0 and freq[i-1]>0:
+                first_min=i
+                freq[i-1]-=1
+            if second_min==0 and freq[i-1]>0:
+                second_min=i
+                break
+        s+=first_min+second_min
+        print(first_min,second_min)
         if s<=money:
             return money-s
+
         
         
         return money
