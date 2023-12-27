1fe27ae0589be0bd6cacaba8362c1d0133ece72e
Keerthi Konduru <71881753+Keerthi-2@users.noreply.github.com>
2023-12-27 11:19:35 +0530

    Time: 827 ms (93.21%) | Memory: 28.5 MB (6.39%) - LeetSync

--- 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.py
+++ 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.py
@@ -1,21 +1,24 @@
 class Solution:
    def minCost(self,c, neededTime):
         n=len(c)
-        dp=[0]*(n+1)
+        previous_cost=0
+        current_cost=0
         previous_color='A'
         previous_time=0
         ans=0
         for i in range(1,n+1):
             if previous_color==c[i-1]:
-                dp[i]=dp[i-1]+min(previous_time,neededTime[i-1])
+                current_cost=previous_cost+min(previous_time,neededTime[i-1])
                 previous_time=max(previous_time,neededTime[i-1])
 
             else:
-                dp[i]=dp[i-1]
+                current_cost=previous_cost
                 previous_color=c[i-1]
                 previous_time=neededTime[i-1]
+            
+            previous_cost=current_cost
         
-        return dp[n]
+        return current_cost
 
 
         
