a6f216a77107e7de275d6db144c94670e96dac55
Keerthi Konduru <71881753+Keerthi-2@users.noreply.github.com>
2023-12-29 10:01:56 +0530

    Time: 3117 ms (5.08%) | Memory: 24.1 MB (5.09%) - LeetSync

---
+++ 1457-minimum-difficulty-of-a-job-schedule/minimum-difficulty-of-a-job-schedule.py
@@ -0,0 +1,26 @@
+class Solution:
+    def minDifficulty(self, jobs: List[int], d: int) -> int:
+
+        if len(jobs)<d:
+            return -1
+        cache={}
+
+        def dfs(i,d,cur_max):
+            if i==len(jobs) :
+                return 0 if d==0 else float('inf')
+            
+            if d==0:
+                return float('inf')
+            if (i,d,cur_max) in cache:
+                return cache[(i,d,cur_max)]
+
+            cur_max=max(cur_max,jobs[i])
+                        #continue the day   
+            res=min(dfs(i+1,d,cur_max),cur_max+dfs(i+1,d-1,-1))
+            cache[(i,d,cur_max)]=res
+
+            return res
+
+        
+        return dfs(0,d,-1)
+        
