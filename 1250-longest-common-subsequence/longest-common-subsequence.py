e3ef7a97dac2fb8f41c5f7ac9ec1ee69c46c9330
Keerthi Konduru <71881753+Keerthi-2@users.noreply.github.com>
2024-01-26 12:19:09 +0530

    Time: 462 ms (88.86%) | Memory: 16.6 MB (95.94%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -1,21 +1,19 @@
 class Solution:
-    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        # Get the lengths of both input strings
-        len_text1, len_text2 = len(text1), len(text2)
-      
-        # Initialize a 2D array (list of lists) with zeros for dynamic programming
-        # The array has (len_text1 + 1) rows and (len_text2 + 1) columns
-        dp_matrix = [[0] * (len_text2 + 1) for _ in range(len_text1 + 1)]
-      
-        # Loop through each character index of text1 and text2
-        for i in range(1, len_text1 + 1):
-            for j in range(1, len_text2 + 1):
-                # If the characters match, take the diagonal value and add 1
-                if text1[i - 1] == text2[j - 1]:
-                    dp_matrix[i][j] = dp_matrix[i - 1][j - 1] + 1
+    def longestCommonSubsequence(self, s1: str, s2: str) -> int:
+
+        n1=len(s1)
+        n2=len(s2)
+
+        dp=[0 for i in range(n2+1)]
+
+        for i in range(1,n1+1):
+            new_dp=[0 for i in range(n2+1)]
+            for j in range(1,n2+1):
+                if s1[i-1]==s2[j-1]:
+                    new_dp[j]=1+dp[j-1]
                 else:
-                    # If the characters do not match, take the maximum of the value from the left and above
-                    dp_matrix[i][j] = max(dp_matrix[i - 1][j], dp_matrix[i][j - 1])
-      
-        # The bottom-right value in the matrix contains the length of the longest common subsequence
-        return dp_matrix[len_text1][len_text2]
+                    new_dp[j]=max(dp[j],new_dp[j-1])
+            dp=new_dp
+        
+        return dp[-1]
+
