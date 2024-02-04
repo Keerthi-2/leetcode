d5923452bb7d63d71d36f452be71477e30e92fd3
Keerthi Konduru <71881753+Keerthi-2@users.noreply.github.com>
2024-02-04 18:55:55 +0530

    Time: 82 ms (90.53%) | Memory: 17.3 MB (76.65%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,39 +1,34 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        #s = "ADOBECODEBANC", t = "ABC"
-        mt = {}
-        n = len(t)
-        for i in range(n):
-            if t[i] in mt:
-                mt[t[i]] += 1
+        d={}
+        for i in t:
+            if i in d:
+                d[i]+=1
             else:
-                mt[t[i]] = 1
-        # A-  1
-        # B - 1
-        # C - 1
-        distinct = len(mt)
-        i=0
-        end=-1
-        start=-1
-        minlength=100000
-        for j in range(len(s)):
-            
-            if s[j] in mt:
-                mt[s[j]]-= 1
-                if mt[s[j]] == 0: distinct -= 1
-            while distinct == 0:
+                d[i]=1
 
-                if (j-i+1)<minlength:
-                    minlength = (j-i+1)
+        distinct=len(d)
+        start=-1
+        end=-1
+        i=0
+        ans=10**7
+        for j in range(len(s)):
+            if s[j] in d:
+                d[s[j]]-=1
+                if d[s[j]]==0:
+                    distinct-=1
+
+            while distinct==0:
+
+                if j-i+1<ans:
+                    ans=j-i+1
                     start=i
                     end=j
-                if s[i] in mt: 
-                    mt[s[i]] += 1
-                    if mt[s[i]] == 1 : distinct += 1
-                
-                i+=1;
-            
-        ans = ""
-    
+                if s[i] in d:
+                    d[s[i]]+=1
+
+                    if d[s[i]]==1:
+                        distinct+=1
+                i+=1
         return s[start:end+1]
-        
+
