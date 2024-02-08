b8587dff4b5b0c2ac20254158441c96e101cbde4
Keerthi Konduru <71881753+Keerthi-2@users.noreply.github.com>
2024-02-08 19:38:35 +0530

    Time: 555 ms (20.87%) | Memory: 20.3 MB (44.78%) - LeetSync

--- 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.py
+++ 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.py
@@ -3,6 +3,11 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def doubleIt(self, head: Optional[ListNode]) -> Optional[ListNode]:
         s=[]
@@ -13,23 +18,24 @@ class Solution:
         new=None
         ans=new
         carry=0
-        while(len(s)):
+        while(len(s) or carry==1):
             if len(s)>0:
                 cur=s.pop()
+            else:
+                cur=0
             cur=cur*2+carry
             print(carry,cur%10)
             new_node=ListNode(cur%10)
             carry=cur//10
             new_node.next=new
             new=new_node
-        if carry==1:
-            new_node=ListNode(1)
-            new_node.next=new
-            new=new_node
+        
         return new
         
         
 
-        return new
+        
+
+        
 
         
