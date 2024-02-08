d4ea1bad577e90908d8a7b58cbc9c3aab71bd2b0
Keerthi Konduru <71881753+Keerthi-2@users.noreply.github.com>
2024-02-08 17:55:06 +0530

    Time: 2941 ms (36.65%) | Memory: 19 MB (24.47%) - LeetSync

--- 279-perfect-squares/perfect-squares.py
+++ 279-perfect-squares/perfect-squares.py
@@ -1,16 +1,19 @@
 class Solution:
     def numSquares(self, n: int) -> int:
-        
+          # 12  1 2 3
         def solve(n,dp):
-            if n==0:
+            if n<=0:
                 return 0
             if dp[n]!=-1:
                 return dp[n]
+            
             ans=10**7
             for i in range(1,int(n**0.5)+1):
                 ans=min(ans,1+solve(n-i*i,dp))
-                dp[n]=ans
+            dp[n]=ans
+            
             return dp[n]
+        
 
 
         dp=[-1]*(n+1)
