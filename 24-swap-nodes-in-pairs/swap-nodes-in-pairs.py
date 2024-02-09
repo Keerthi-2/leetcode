7b3a6bef606936d953bd121b104cbfa53c0f2299
Keerthi Konduru <71881753+Keerthi-2@users.noreply.github.com>
2024-02-09 18:44:53 +0530

    Time: 41 ms (30.14%) | Memory: 16.5 MB (66.08%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.py
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.py
@@ -5,19 +5,23 @@
 #         self.next = next
 class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if head==None or head.next is None:
+        if head is None or head.next is None:
             return head
         def recursion(cur):
-            if cur==None or  cur.next==None:
-                return cur
 
+            if cur is None or cur.next is None:
+                return cur
             end=cur.next
+
             nex=end.next
             end.next=cur
 
+
             cur.next=recursion(nex)
             return end
+        
+
+
 
 
         return recursion(head)
-        
