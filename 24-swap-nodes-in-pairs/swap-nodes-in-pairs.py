8497d8781ef6ac687d692856e72b9044e876b9f6
Keerthi Konduru <71881753+Keerthi-2@users.noreply.github.com>
2024-02-09 19:25:42 +0530

    Time: 42 ms (25.60%) | Memory: 16.5 MB (66.08%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.py
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.py
@@ -3,25 +3,20 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if head is None or head.next is None:
-            return head
-        def recursion(cur):
-
-            if cur is None or cur.next is None:
-                return cur
-            end=cur.next
-
-            nex=end.next
-            end.next=cur
-
-
-            cur.next=recursion(nex)
-            return end
-        
-
-
-
-
-        return recursion(head)
+        s = ListNode(0)
+        s.next = head
+        l = s
+        while l.next and l.next.next :
+            rr = l.next.next
+            l.next.next = l.next.next.next
+            rr.next = l.next
+            l.next = rr
+            l = l.next.next
+        return s.next     
