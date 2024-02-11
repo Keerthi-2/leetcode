3f4528ed37a02dcd68a6f0292c809430366b4016
Keerthi Konduru <71881753+Keerthi-2@users.noreply.github.com>
2024-02-11 18:23:32 +0530

    Time: 42 ms (76.96%) | Memory: 16.8 MB (60.99%) - LeetSync

---
+++ 682-baseball-game/baseball-game.py
@@ -0,0 +1,18 @@
+class Solution:
+    def calPoints(self, op: List[str]) -> int:
+        st=[]
+        for i in op:
+            if i=='D':
+                top=st[-1]*2
+                st.append(top)
+            elif i=='C':
+                st.pop()
+            elif i=='+':
+                if len(st)>=2:
+                    top=st[-1]+st[-2]
+                    st.append(top)
+            else:
+                st.append(int(i))
+        return sum(st)
+
+        
