b01ad71ea32c8e44be7b5f574e1c653229bb6000
Keerthi Konduru <71881753+Keerthi-2@users.noreply.github.com>
2024-02-24 23:09:46 +0530

    Time: 923 ms (54.29%) | Memory: 28.1 MB (91.56%) - LeetSync

--- 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.py
+++ 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.py
@@ -1,61 +1,22 @@
-class TrieNode:
-    
-    def __init__(self):
-        self.node=[0]*10
-        self.flag=False
-    
-    def containskey(self,ch):
-        
-        if self.node[ord(ch)-48]!=0:
-            return True
-        return False
-    
-    def put(self,ch,reference_node):
-        self.node[ord(ch)-48]=reference_node
-    def get(self,ch):
-        return self.node[ord(ch)-48]
-    def setend(self):
-        self.flag=True
-    def isend(self):
-        return self.flag
-
-
 class Solution:
-    def __init__(self):
-        self.root=TrieNode()
     def longestCommonPrefix(self, arr1: List[int], arr2: List[int]) -> int:
         
+        hs=set()
+
+        for i in arr1:
+            cur=str(i)
+            s=''
+            for j in range(len(cur)):
+                s+=cur[j]
+                hs.add(s)
+        
         ans=0
-        
-        
-        for cur in arr1:
-            s=str(cur)
-            node=self.root
-            for i in range(len(s)):
-                if((node.containskey(s[i]))==False):
-                    node.put(s[i],TrieNode())
-                node=node.get(s[i])
-            node.setend()
-        
-        
-        for cur in arr2:
-            s2=str(cur)
-            node=self.root
-            steps=0
-            for i in range(len(s2)):
-                
-                if node.containskey(s2[i])==False:
-                    break
-                else:
-                    node=node.get(s2[i])
-                steps+=1
-            print(cur,i,node.isend())
-            
-            ans=max(ans,steps)
-            
-        
+        for i in arr2:
+            cur=''
+            st=str(i)
+            for j in st:
+                cur+=j
+                if cur in hs:
+                    ans=max(ans,len(cur))
+
         return ans
-            
-                
-            
-        
