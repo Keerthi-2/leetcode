aeac6be512d41a552e0fcbfd945ca0e6bfa71893
Keerthi Konduru <71881753+Keerthi-2@users.noreply.github.com>
2024-03-02 18:44:13 +0530

    Time: 170 ms (15.37%) | Memory: 18.6 MB (84.68%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -1,8 +1,34 @@
 class Solution:
-    def sortedSquares(self, nums: List[int]) -> List[int]:
-        for i in range(len(nums)):
-            nums[i]*=nums[i]
+    def sortedSquares(self,arr: List[int]) -> List[int]:
+        def binarysearch(arr,l,h):
+            while(l<=h):
+                mid=(l+h)//2
+                if arr[mid]<0:
+                    l=mid+1
+                else:
+                    h=mid-1
+            return h
+        res=[]
+        n=len(arr)
+        cur=binarysearch(arr,0,n-1)
+        print(cur)
+
+        j=cur
+        cur+=1
+
+        while(j>=0 and cur<n):
+            if arr[j]*arr[j]<=arr[cur]*arr[cur]:
+                res.append(arr[j]*arr[j])
+                j-=1
+            else:
+                res.append(arr[cur]*arr[cur])
+                cur+=1
         
-        nums.sort()
-        return nums
+        while(j>=0):
+            res.append(arr[j]*arr[j])
+            j-=1
+        while(cur<n):
+            res.append(arr[cur]*arr[cur])
+            cur+=1
         
+        return res
