1dff54c6c9a717ae7c98175c7e11a1891579951e
Keerthi Konduru <71881753+Keerthi-2@users.noreply.github.com>
2024-05-20 11:05:04 +0530

    Time: 41 ms (78.60%) | Memory: 16.6 MB (48.58%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.py
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.py
@@ -1,29 +1,8 @@
 class Solution:
     def subsetXORSum(self, nums: List[int]) -> int:
-        def bt(node,path):
-            if node<=n:
-                sub.append(path.copy())
-            
-            for i in range(node,n):
-                path.append(nums[i])
-                bt(i+1,path)
-                path.pop()
-        res=0
-        n=len(nums)
-        sub=[]
-        bt(0,[])
-        print(sub)
-        for i in sub:
-            val=0
-            for j in i:
-                val=val^j
-            
-            res+=val
-        
-        return res
-
-
-
-
-
-        
+        result = 0
+        # Capture each bit that is set in any of the elements
+        for num in nums:
+            result |= num
+        # Multiply by the number of subset XOR totals that will have each bit set
+        return result << (len(nums) - 1)
