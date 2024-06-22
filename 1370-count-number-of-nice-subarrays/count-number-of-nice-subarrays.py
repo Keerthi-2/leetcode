4443595a40d5c003b28414821dd6bac2b4efb624
Keerthi Konduru <71881753+Keerthi-2@users.noreply.github.com>
2024-06-22 09:34:28 +0530

    Time: 647 ms (29.78%) | Memory: 23.5 MB (84.95%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.py
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.py
@@ -1,16 +1,15 @@
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
-        
-        curr_sum = 0
-        subarrays = 0
-        prefix_sum = {curr_sum: 1}
+        return self.atMost(nums, k) - self.atMost(nums, k - 1)
 
-        for i in range(len(nums)):
-            curr_sum += nums[i] % 2
-            # Find subarrays with sum k ending at i
-            if curr_sum - k in prefix_sum:
-                subarrays = subarrays + prefix_sum[curr_sum - k]
-            # Increment the current prefix sum in hashmap
-            prefix_sum[curr_sum] = prefix_sum.get(curr_sum, 0) + 1
-
+    def atMost(self, nums: List[int], k: int) -> int:
+        window_size, subarrays, start = 0, 0, 0
+        for end in range(len(nums)):
+            window_size += nums[end] % 2
+            # Find the first index start where the window has exactly k odd elements.
+            while window_size > k:
+                window_size -= nums[start] % 2
+                start += 1
+            # Increment number of subarrays with end - start + 1.
+            subarrays += end - start + 1
         return subarrays
