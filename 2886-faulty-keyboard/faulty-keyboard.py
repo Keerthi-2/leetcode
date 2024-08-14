5de79f29d3e0a7eda4ece80b33e48ab5934c775c
Keerthi Konduru <71881753+Keerthi-2@users.noreply.github.com>
2024-08-14 12:13:28 +0530

    Time: 55 ms (10.70%) | Memory: 16.5 MB (75.93%) - LeetSync

--- 2886-faulty-keyboard/faulty-keyboard.py
+++ 2886-faulty-keyboard/faulty-keyboard.py
@@ -1,18 +1,23 @@
 class Solution:
     def finalString(self, s: str) -> str:
-        # process from the right end, write from both ends
-        n = len(s)
-        result = [""]*n
-        left, right = 0, n-1
-        parity = 1
+        
+        n=len(s)
+        res=[""]*n
+        l,r=0,n-1
+        flag=1
         for i in range(n-1,-1,-1):
-            l = s[i]
-            if l == "i": 
-                parity = 1 - parity
-            elif parity == 1:
-                result[right] = l
-                right -= 1
+            cur=s[i]
+            
+            if cur=='i':
+                flag=flag^1
+            elif flag==1:
+                res[r]=cur
+                r-=1
             else:
-                result[left] = l
-                left += 1
-        return "".join(result)
+                res[l]=cur
+                l+=1
+        print(res)
+        return "".join(res)
+                
+            
+        
