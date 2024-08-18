0832a2c79944381e2869e02bba59387f3cba7c0c
Keerthi Konduru <71881753+Keerthi-2@users.noreply.github.com>
2024-08-18 11:36:58 +0530

    Time: 481 ms (7.12%) | Memory: 16.9 MB (11.04%) - LeetSync

--- 264-ugly-number-ii/ugly-number-ii.py
+++ 264-ugly-number-ii/ugly-number-ii.py
@@ -15,16 +15,15 @@ class Solution:
             cur=heappop(l)
             print(cur)
             
-            if cur*2 not in s:
+            if cur*2 not in l:
                 heappush(l,cur*2)
-                s.add(cur*2)
-            if cur*3 not in s:
-                heappush(l,cur*3)
-                s.add(cur*3)
-            if cur*5 not in s:
-                heappush(l,cur*5)
-                s.add(cur*5)
             
+            if cur*3 not in l:
+                heappush(l,cur*3)
+                
+            if cur*5 not in l:
+                heappush(l,cur*5)
+                
             
             
             n-=1
