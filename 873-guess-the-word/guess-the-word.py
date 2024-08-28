c56f1f7773cb97f64b526b009cc5a38525f48d3d
Keerthi Konduru <71881753+Keerthi-2@users.noreply.github.com>
2024-08-28 23:46:59 +0530

    Time: 34 ms (77.34%) | Memory: 16.6 MB (46.80%) - LeetSync

--- 873-guess-the-word/guess-the-word.py
+++ 873-guess-the-word/guess-the-word.py
@@ -4,36 +4,29 @@
 # """
 # class Master:
 #     def guess(self, word: str) -> int:
-import random
+
 class Solution:
     def findSecretWord(self, words: List[str], master: 'Master') -> None:
-        #APPROACH2
-        #Since we may find secret word after 10 guesses,
-        #1. Stop aiming to find secret word
-        #2. Think of algorithm to make 10 calls fully to master.guess or until we find secret word, whichever comes first  
-        #Match count between guess word and every candidate in wordslist
-        def guessMatches(guessWord, candidate, count):
-            matches = 0
-            for g,c in zip(guessWord, candidate):
-                if g == c:
-                    matches += 1
-            return count == matches
-
-        matches = 0
-        i = 0
-        #Iterate until 10 guesses
-        while i < 10 and matches != 6:
-            word = words[random.randint(0, len(words)-1)]
-            matches = master.guess(word)
-            candidates = []
-            #Eliminate candidates that won't match with guessed word (which have common chars with secret)
-            for cand in words:
-                if guessMatches(word, cand, matches):
-                    candidates.append(cand)
-            words = candidates
-        # return word
-    
-        #APPROCH 1
-        #BRUTEFORCE - Cannot pass all testcases with 10 guesses
-        # for word in words:
-        #     res = master.guess(word)
+        l=0
+        i=0
+        while(i<10 and l!=6):
+            
+            cur=words[random.randint(0, len(words)-1)]
+           
+            
+            l=master.guess(cur)
+            
+            if l==6:
+                return
+            
+            
+            newl=[]
+            for word in words:
+                cnt=0
+                for g,c in zip(word, cur):
+                    if g == c:
+                        cnt += 1
+                if cnt==l:
+                    newl.append(word)
+            
+            words=newl
