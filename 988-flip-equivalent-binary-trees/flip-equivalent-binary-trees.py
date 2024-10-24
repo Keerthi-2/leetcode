504b1480743e7f9d6e9d07c667744621d0ca0f32
Keerthi Konduru <71881753+Keerthi-2@users.noreply.github.com>
2024-10-24 19:39:18 +0530

    Time: 0 ms (100.00%) | Memory: 16.6 MB (16.61%) - LeetSync

---
+++ 988-flip-equivalent-binary-trees/flip-equivalent-binary-trees.py
@@ -0,0 +1,25 @@
+class Solution:
+    def flipEquiv(
+        self, root1: Optional[TreeNode], root2: Optional[TreeNode]
+    ) -> bool:
+        # Both trees are empty
+        if not root1 and not root2:
+            return True
+        # Just one of the trees is empty
+        if not root1 or not root2:
+            return False
+        # Corresponding values differ
+        if root1.val != root2.val:
+            return False
+
+        # Check if corresponding subtrees are flip equivalent
+        no_swap = self.flipEquiv(root1.left, root2.left) and self.flipEquiv(
+            root1.right, root2.right
+        )
+
+        # Check if opposite subtrees are flip equivalent
+        swap = self.flipEquiv(root1.left, root2.right) and self.flipEquiv(
+            root1.right, root2.left
+        )
+
+        return no_swap or swap
