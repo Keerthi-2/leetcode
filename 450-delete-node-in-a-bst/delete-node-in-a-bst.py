f7fffb3b8ed9d5f38a554d5d7d86eb0776e8ae55
Keerthi Konduru <71881753+Keerthi-2@users.noreply.github.com>
2025-01-20 21:59:14 +0530

    Time: 0 ms (100.00%) | Memory: 21.4 MB (19.69%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.py
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.py
@@ -1,63 +1,25 @@
 class Solution:
     def deleteNode(self, A: Optional[TreeNode], B: int) -> Optional[TreeNode]:
-        if not A:
-            return A
-        
-        parent=None
-        cur=A
-        
-        while(cur and cur.val!=B):
-            parent=cur
-            if B<cur.val:
-                cur=cur.left
-            elif B>cur.val:
-                cur=cur.right
+        if(A):
+            if(B < A.val): 
+                A.left = self.deleteNode(A.left, B)                 
+            elif(B > A.val): 
+                A.right = self.deleteNode(A.right, B)       
+            else:
+                if(not A.left and not A.right): 
+                    return None             
+                if (not A.left or not A.right):
+                    return A.left if A.left else A.right           
+                                                                  
+                temp = A.left                         
+                while(temp.right != None): 
+                    temp = temp.right 
 
-        if cur is None:
-            return A       
-        #print(cur.val)
-        if cur.left is None and cur.right is None:
-            #print("no child")
-            if parent is None:
-                #print("oh")
-                return None
-                
-            if parent.left==cur:
-                parent.left=None
-            else:
-                parent.right=None
-                
-        
-        elif cur.left is None or cur.right is None:
-            #print("one child")
-            child=None
-            if cur.left:
-                child=cur.left
-            else:
-                child=cur.right
+                temp.right=A.right
+                head= A.left
+                A.left=None
+                return head
+                # A.val = temp.val                     
+                # A.left = self.solve(A.left, temp.val)
             
-            if parent is None:
-                return child
-            
-            if parent.left==cur:
-                parent.left=child
-            else:
-                parent.right=child
-                
-        else:
-          #  print("two child")
-            left_node=cur.left
-            prev_pred=None
-            while(left_node.right is not None):
-                prev_pred=left_node
-                left_node=left_node.right
-                
-            cur.val=left_node.val
-            
-            if prev_pred is None:
-                cur.left=left_node.left
-            else:
-                prev_pred.right=left_node.left
-                
-        
-        return A
+        return A;
